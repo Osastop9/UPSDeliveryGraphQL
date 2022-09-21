c6bc72dbce4717eecf7690b00542414d3aff7b48
Isaac Uduehi <osayi4jesus@gmail.com>
2022-09-21 02:17:25 +0100

    custom hooks

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-
 import RootNavigator from "./navigator/RootNavigator";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5001/api/cold-bunny",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
